63cfc37d45d9c32079780f60cf1fcf04be60bbec
nikita Levchenko <nikitos13711@gmail.com>
2025-06-05 15:13:19 +0300

    fix number input font size and min value

--- src/features/calc/view/parts/NumberField.tsx
+++ src/features/calc/view/parts/NumberField.tsx
@@ -12,9 +12,9 @@ export const NumberField = ({ isValid, label, onChange, value, errorText }: Prop
   return (
     <Field.Root invalid={isValid}>
       <Field.Label fontWeight="semibold">{label}</Field.Label>
-      <NumberInput.Root width="100%" value={value} onValueChange={(e) => onChange(e.value)}>
+      <NumberInput.Root min={0} width="100%" value={value} onValueChange={(e) => onChange(e.value)}>
         <NumberInput.Control />
-        <NumberInput.Input placeholder="10" />
+        <NumberInput.Input fontSize={16} placeholder="10" />
       </NumberInput.Root>
       <Field.ErrorText>{errorText}</Field.ErrorText>
     </Field.Root>
