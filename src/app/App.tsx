22a3f44e13ea0f31e7a65b063394f5bbc046f8d5
nikita Levchenko <nikitos13711@gmail.com>
2025-06-05 15:13:25 +0300

    fix theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
 import { Provider } from '@/components/ui/provider'
 import { MainPage } from '@/pages/MainPage'
+import { Theme } from '@chakra-ui/react'
 
 import '@/features/calc/model/init'
 
 export const App = () => {
   return (
     <Provider>
-      <MainPage />
+      <Theme appearance="light">
+        <MainPage />
+      </Theme>
     </Provider>
   )
 }
